cc4c8d325237680d45f6039993dbf92d9ab8ba10
Ephraim Gathoni <ephnjor@gmail.com>
2023-05-09 15:02:30 -0700

    task 6

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-for i in range(10):
-    for j in range(10):
-        if(i is 8 and j is 9):
-            print("{}".format(str(i) + str(j)))
-        elif(j > i):
-            print("{}".format(str(i) + str(j)) + ", ", end="")
+number = 0
+while number <= 89:
+    if number % 10 == 0:
+        number += 1 + number // 10
+    print("{:02d}".format(number), end='\n' if number == 89 else ", ")
+    number += 1
