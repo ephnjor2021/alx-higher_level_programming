c588a9cf549f425733131a822085c78560d2841d
Ephraim Gathoni <ephnjor@gmail.com>
2023-05-30 05:11:29 -0700

    0-square.py

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,12 +1,6 @@
 #!/usr/bin/python3
-
-
 class Square:
-    """An empty class Square."""
     pass
-
-
-if __name__ == '__main__':
-    my_square = Square()
-    print(type(my_square))
-    print(my_square.__dict__)
+    """
+    creates a square class object
+    """
