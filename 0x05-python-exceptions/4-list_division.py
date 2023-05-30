f3774521060305529054978bee39cff8442c072f
Ephraim Gathoni <ephnjor@gmail.com>
2023-05-30 04:15:37 -0700

    4-list_division.py

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -1,16 +1,39 @@
 #!/usr/bin/python3
+
+
+def safe_division(a, b):
+    quotient = 0
+    try:
+        quotient = a / b
+    except ZeroDivisionError:
+        print("division by 0")
+    except TypeError:
+        print("wrong type")
+    finally:
+        return quotient
+
+
 def list_division(my_list_1, my_list_2, list_length):
-    newList = []
-    for x in range(list_length):
-        result = 0
+    result_li = []
+    for i in range(list_length):
         try:
-            result = my_list_1[x] / my_list_2[x]
+            result_li.append(safe_division(my_list_1[i], my_list_2[i]))
         except IndexError:
-            print("{}".format("out of range"))
-        except (ValueError, TypeError):
-            print("{}".format("wrong type"))
-        except ZeroDivisionError:
-            print("{}".format("division by 0"))
-        finally:
-            newList.append(result or 0)
-    return(newList)
+            print("out of range")
+            result_li.append(0)
+
+    return result_li
+
+
+if __name__ == '__main__':
+    my_l_1 = [10, 8, 4]
+    my_l_2 = [2, 4, 4]
+    result = list_division(my_l_1, my_l_2, max(len(my_l_1), len(my_l_2)))
+    print(result)
+
+    print("--")
+
+    my_l_1 = [10, 8, 4, 4]
+    my_l_2 = [2, 0, "H", 2, 7]
+    result = list_division(my_l_1, my_l_2, max(len(my_l_1), len(my_l_2)))
+    print(result)
