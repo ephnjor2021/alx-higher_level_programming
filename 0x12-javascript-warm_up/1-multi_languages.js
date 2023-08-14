0edf556738d6108051dfa4c10284dea454cf20ba
Ephraim <ephnjor@gmail.com>
2023-08-14 06:44:50 -0700

    1-multi_languages.js

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 let i = 0;
-const arr = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+const arr = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 
 for (i = 0; i < arr.length; i++) {
   console.log(arr[i]);
