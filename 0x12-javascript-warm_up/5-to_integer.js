68bf53555a6d572717d83ddd646145fd79f82229
Ephraim <ephnjor@gmail.com>
2023-08-14 08:05:39 -0700

    5-to_integer.js

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
-let args = process.argv.slice(2);
-if (isNaN(Number(args[0]))) {
+const myArgs = process.argv.slice(2);
+if (isNaN(myArgs[0])) {
   console.log('Not a number');
 } else {
-  console.log('My number: ' + parseInt(args[0]));
+  console.log('My number: ' + myArgs[0]);
 }
