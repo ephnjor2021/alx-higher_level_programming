ce2b8b834449b9a47a3232a946f7489e7d619d6b
Ephraim <ephnjor@gmail.com>
2023-08-15 04:20:04 -0700

    4-rectangle.js

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -1,32 +1,25 @@
 #!/usr/bin/node
-
-class Rectangle {
+// The constructor is used to stablished the object properties or to call the methods to prepare the object for use
+module.exports = class Rectangle {
   constructor (w, h) {
     if (w > 0 && h > 0) {
       this.width = w;
       this.height = h;
     }
   }
-  print () {
-    let rows, columns;
 
-    for (rows = 0; rows < this.height; rows++) {
-      let str = '';
-      for (columns = 0; columns < this.width; columns++) {
-        str += 'X';
-      }
-      console.log(str);
+  print () {
+    for (let t = 0; t < this.height; t++) {
+      console.log('X'.repeat(this.width));
     }
   }
-  rotate () {
-    let temp = this.width;
-    this.width = this.height;
-    this.height = temp;
-  }
-  double () {
-    this.width *= 2;
-    this.height *= 2;
-  }
-}
 
-module.exports = Rectangle;
+  rotate () {
+    [this.height, this.width] = [this.width, this.height];
+  }
+
+  double () {
+    this.width = this.width * 2;
+    this.height = this.height * 2;
+  }
+};
