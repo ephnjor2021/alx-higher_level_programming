a83b36cf81205229a274c8ac3ebd5b739cbd532b
Ephraim <ephnjor@gmail.com>
2023-08-31 09:48:02 -0700

    6-peak.py

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,17 +1,10 @@
 #!/usr/bin/python3
-# finds peak
-
-
-def search(lo, h, ints):
-    mid = (lo + h) // 2
-    if lo == h:
-        return ints[h]
-    if ints[mid] < ints[mid + 1]:
-        return(search(mid + 1, h, ints))
-    return(search(lo, mid, ints))
+"""Write a function that finds a peak in a list of unsorted integers."""
 
 
 def find_peak(list_of_integers):
-    if not list_of_integers:
-        return
-    return(search(0, len(list_of_integers) - 1, list_of_integers))
+    """[summary]"""
+    if list_of_integers:
+        list_of_integers.sort()
+        return list_of_integers[-1]
+    return None
