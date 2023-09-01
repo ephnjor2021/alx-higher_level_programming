efce21bb59cdcc01e6b33d9c127cd53e1a130d9d
Ephraim <ephnjor@gmail.com>
2023-09-01 09:58:28 -0700

    4-hbtn_status.py

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
-""" script that fetches https://intranet.hbtn.io/status using requests """
-if __name__ == '__main__':
-    import requests
+# requests use
+import requests
 
-    response = requests.get('https://intranet.hbtn.io/status')
-    print('Body response:')
-    print('\t- type: {}'.format(type(response.text)))
-    print('\t- content: {}'.format(response.text))
+
+if __name__ == "__main__":
+    r = requests.get('https://intranet.hbtn.io/status')
+    print("Body response:")
+    print("\t- type: {}".format(type(r.text)))
+    print("\t- content: {}".format(r.text))
